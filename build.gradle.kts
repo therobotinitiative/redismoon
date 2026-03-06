405628a6040acee6be362ec18c962ab7fa3d5b03
Mikko Siren <mikko.juhani.siren@gmail.com>
2026-03-06 20:50:13 +0200

    Project build fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.orbital3d.gradle.moon.base") version "0.1.0-local"
-    id("com.orbital3d.gradle.moon.docker") version "0.1.0-local"
+    id("com.orbital3d.gradle.moon.base") version "0.1.1-SNAPSHOT"
+    id("com.orbital3d.gradle.moon.docker") version "0.1.1-SNAPSHOT"
 }
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REDIS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-source "$REDIS_DIR/../bash/forest.functions.sh"
+source "$REDIS_DIR/../forestbash/forest.functions.sh"
 
 # Load gradle.properties into associative array
 declare -A properties
@@ -12,8 +12,8 @@ forestnetwork=$(forest_resolve_network_name "$REDIS_DIR/../gradle.properties")
 # Run the docker container for Redis
 log_lgreen "Starting Redis Moon Container"
 docker run -d --name redis-moon \
-                -p "${properties[redis.port]}":6379 \
+                -p "${properties[redismoon.port]}":6379 \
                 --network="${forestnetwork}" \
-                "${properties[redis.image.name]}":"${properties[redis.image.version]}"
+                "${properties[redismoon.image.name]}":"${properties[redismoon.image.version]}"
 log_lgreen "Redis Moon Container started"
 log_nc ""
